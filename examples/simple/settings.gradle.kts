44d9a62cc01495d7c65214a09818e3474013b166
Anders Bendtsen <anders.bendtsen@consid.com>
2025-06-17 15:39:17 +0200

    Add first simple demo

---
+++ examples/simple/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "agentic-ai-kotlin"
---
+++ examples/simple/src/main/kotlin/com/consid/agenticai/demos/simple/AgenticAiApplication.kt
@@ -0,0 +1,11 @@
+package com.consid.agenticai.demos.simple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgenticAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<AgenticAiApplication>(*args)
+}
