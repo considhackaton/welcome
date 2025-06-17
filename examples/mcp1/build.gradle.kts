90597f800941a05c21d938aeb99cfbe390c8dc11
Anders Bendtsen <anders.bendtsen@consid.com>
2025-06-17 15:46:52 +0200

    Add .gitignore, create new mcp1

--- examples/mcp1/build.gradle.kts
+++ examples/mcp1/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.23"
 }
 
-group = "com.consid.agenticai.demos.simple"
+group = "com.consid.agenticai.demos.mcp1"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
